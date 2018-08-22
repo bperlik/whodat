c321be6fea7a35f148914a84667bd29eb16194c6
Barbara Perlik <barbperlik@gmail.com>
2018-08-22 10:12:48 -0400

    Modify for RSpec Testing'

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,6 +1,15 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+require "active_record/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 require "whodat"
@@ -8,7 +17,8 @@ require "whodat"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    
+    # config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
